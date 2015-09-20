72e50a11fd70c83723a3c7256d3146e0f9b660fb
Zak Fisher <zak.fisher@gmail.com>
2015-09-19 17:45:27 -0700

    setup for base architecture

---
+++ routes.jsx
@@ -0,0 +1,34 @@
+Router.route('/', function() {
+  document.title = 'Togger - Login'
+  this.render('Login')
+})
+
+Router.route('/home', function() {
+  document.title = 'Togger - Home'
+  this.render('Home')
+})
+
+Router.route('/payment', function() {
+document.title = 'Togger - Payment'
+  this.render('Payment')
+})
+
+Router.route('/connect', function() {
+  document.title = 'Togger - Connect'
+  this.render('Connect')
+})
+
+Router.route('/chat', function() {
+  document.title = 'Togger - Chat'
+  this.render('Chat')
+})
+
+Router.route('/rate', function() {
+  document.title = 'Togger - Rate'
+  this.render('Rate')
+})
+
+Router.route('/share', function() {
+  document.title = 'Togger - Share'
+  this.render('Share')
+})
